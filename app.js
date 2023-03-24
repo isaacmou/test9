f77dd23a57cffb641deaef77adb948a4d5550ce5
isaacmou <126214425+isaacmou@users.noreply.github.com>
2023-03-24 22:30:54 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+function showEnglish() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "none";
+    document.getElementById("toptitle").innerHTML = "Fitness Walking Tracks";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "none";
+    
+    
+}
+
+function showChinese() {
+    document.getElementById("engVer").style.display = "none";
+    document.getElementById("chiVer").style.display = "";
+    document.getElementById("toptitle").innerHTML = "健步行徑";
+    document.getElementById("engInput").style.display = "";
+    document.getElementById("chiInput").style.display = "none";
+    document.getElementById("engVerwithdistrict").style.display = "none";
+  
+   
+
+}
+
